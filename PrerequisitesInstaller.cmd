ca9ed80beced29ea3a7221e3dc8a3a3523a009ad
vignesh2295 <34911519+vignesh2295@users.noreply.github.com>
2018-02-13 19:20:54 +0530

    Add files via upload

---
+++ PrerequisitesInstaller.cmd
@@ -0,0 +1,11 @@
+prerequisiteinstaller.exe
+/SQLNCLi:prerequisiteinstallerfiles\sqlncli.msi
+/PowerShell:prerequisiteinstallerfiles\Windows6.1-KB2506143-x64.msu
+/NETFX:prerequisiteinstallerfiles\dotNetFx45_Full_x86_x64.exe
+/IDFX:prerequisiteinstallerfiles\Windows6.1-KB974405-x64.msu
+/Sync:prerequisiteinstallerfiles\Synchronization.msi
+/AppFabric:prerequisiteinstallerfiles\WindowsServerAppFabricSetup_x64.exe
+/IDFX11:prerequisiteinstallerfiles\MicrosoftIdentityExtensions-64.msi
+/MSIPCClient:prerequisiteinstallerfiles\setup_msipc_x64.msi
+/WCFDataServices:prerequisiteinstallerfiles\WcfDataServices.exe /quiet 
+/KB2671763:prerequisiteinstallerfiles\AppFabric1.1-RTM-KB2671763-x64-ENU.exe
